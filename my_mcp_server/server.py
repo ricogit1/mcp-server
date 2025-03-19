ba488152a2e1825886c596b618b086cf098cab44
ricogit1 <107574202+ricogit1@users.noreply.github.com>
2025-03-19 17:41:16 +0100

    Updated project

--- my_mcp_server/server.py
+++
@@ -1,9 +0,0 @@
-from mcp.server.fastmcp.server import MCPServer 
-
-server = MCPServer(name="MyMCPServer")
-
-@server.tool
-def query_model(prompt: str) -> str:
-    return f'Received prompt: {prompt}'
-
-server.run()
